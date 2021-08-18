53a5b28da9a45317c623c04e93e8a1749b57ec7e
prasanna888 <prasannavenkatesh888@gmail.com>
2021-08-19 00:29:07 +0530

    Create App.js

---
+++ restaurant-menu/src/App.js
@@ -0,0 +1,11 @@
+import axios from "axios";import { useRef, useState } from "react";import { Switch, Route, useLocation } from "react-router-dom";
+import RestaurantInfo from "./Components/restaurant-list/restaurant-info";import RestaurantDetails from "./Components/restaurant-list/restaurant-details/restaurant-details";
+import './App.css';
+const authUrl = "508ab21d1d779c46f1ac0e70d12df02e";axios.interceptors.request.use(  config => {    config.headers["X-API-KEY"] = authUrl;    return config;  },  error => {    return Promise.reject(error);  });
+
+function App() {
+  const [searchValue, updateSearchValue] = useState("");  const resInfoRef = useRef(null);  const location = useLocation();
+
+  return (    <div className="App">      <header className="App-header">        <div id="inputWrapper">          <h1>Restaurant</h1>          {            location?.pathname !== "/" ? null : (              <>                <input value={searchValue} onChange={(e) => updateSearchValue(e.target.value)} className="search-box" id="input" type="search" autoComplete="off" placeholder="Search by Restaurant Name" />                <button onClick={() => resInfoRef?.current?.searchData(searchValue)} className="search-button">Search </button>              </>            )          }        </div>      </header>
+      <Switch>        <Route exact path="/">          <RestaurantInfo ref={resInfoRef} clearValue={() => updateSearchValue("")} />        </Route>        <Route path="/restaurant/:id" component={RestaurantDetails} />      </Switch>    </div>  );}
+export default App;
